75f6abc56a2d0d9ee48549bf96a969b0e7aedf1e
narko <6020peaks@gmail.com>
2019-08-20 21:04:08 +0200

    Adjusted db properties and cleaned imports

--- P03-ReviewsAPI-final/src/main/java/com/udacity/course3/reviews/controller/CommentsController.java
+++ P03-ReviewsAPI-final/src/main/java/com/udacity/course3/reviews/controller/CommentsController.java
@@ -9,10 +9,8 @@ import com.udacity.course3.reviews.repository.ProductRepository;
 import com.udacity.course3.reviews.repository.ReviewInfoRepository;
 import com.udacity.course3.reviews.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.List;
 import java.util.Optional;
